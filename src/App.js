005824f2a2151011a28c0a4e2c37c0886260948a
azulm95 <azulm95@gmail.com>
2022-09-27 13:27:31 -0500

    template applied
    template applied

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import NavBar from "./components/Navbar";
-import { InfoAboutUs } from "./views/AboutUs"
-import { Container, Col, Row } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/NavBar.css'
+import NavBar from './components/Navbar';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "./assets/css/bootstrap.min.css";
+import "./assets/scss/now-ui-kit.scss?v=1.5.0";
+import "./assets/demo/demo.css?v=1.5.0";
+import "./assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
+// pages for this kit
+import Index from "./views/Index.js";
+import NucleoIcons from "./views/NucleoIcons.js";
+import LoginPage from "./views/examples/LoginPage.js";
+import LandingPage from "./views/examples/LandingPage.js";
+import ProfilePage from "./views/examples/ProfilePage.js";
+
 function App() {
-  const Box = props => <div className="box">{props.children} </div>;
   return (
     <div className="App">
-      <Container>
-      <Row>
-          <Col>
-            <Box>  
-              <NavBar /> 
-            </Box>
-          </Col>
-      </Row>
-      <Row>
-        <Col>
-          <InfoAboutUs/>
-        </Col>
-      </Row>
-        <Row>
-          <Col>
-            <InfoAboutUs/>
-          </Col>
-        </Row>
-      </Container>
+      <NavBar/>
+     <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={(props) => <Index {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={(props) => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/landing-page"
+          render={(props) => <LandingPage {...props} />}
+        />
+        <Route
+          path="/profile-page"
+          render={(props) => <ProfilePage {...props} />}
+        />
+        <Route
+          path="/login-page"
+          render={(props) => <LoginPage {...props} />}
+        />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+      </Switch>
+    </Switch>
+    </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./assets/css/now-ui-kit.css";
+import "./assets/demo/demo.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
